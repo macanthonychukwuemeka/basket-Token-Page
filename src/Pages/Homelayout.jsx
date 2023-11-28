dc19591e4db9c4782a44563a283406e28f9c6da7
MAC-ANTHONY <macanthonygreat@gmail.com>
2023-11-28 21:10:21 +0000

    adjusted card size

--- src/Pages/Homelayout.jsx
+++ src/Pages/Homelayout.jsx
@@ -5,7 +5,7 @@ import SwapPage from "./SwapPage";
 const Homelayout = () => {
   return (
     <div className=" bg-black text-primary-foreground relative overflow-y-auto overflow-x-hidden">
-      {/* <div className="sm:hidden fixed;z-index:-1;left:-10px">
+      {/* <div className="sm:hidden fixed;z-index:-1; ">
         <img
           src="https://storage.googleapis.com/dexhunter-images/public/test_bg_left.svg"
           alt="left_bg"
