ced4e4d2555744866647202a0601c173959a5886
MAC-ANTHONY <macanthonygreat@gmail.com>
2023-11-28 20:40:54 +0000

    new commit

--- src/Pages/Homelayout.jsx
+++ src/Pages/Homelayout.jsx
@@ -5,12 +5,12 @@ import SwapPage from "./SwapPage";
 const Homelayout = () => {
   return (
     <div className=" bg-black text-primary-foreground relative overflow-y-auto overflow-x-hidden">
-      <div className="sm:hidden fixed;z-index:-1;left:-10px">
+      {/* <div className="sm:hidden fixed;z-index:-1;left:-10px">
         <img
           src="https://storage.googleapis.com/dexhunter-images/public/test_bg_left.svg"
           alt="left_bg"
         ></img>
-      </div>
+      </div> */}
       <Navbar />
       <SwapPage />
       <BasketPage />
